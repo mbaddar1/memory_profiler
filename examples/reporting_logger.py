027893db379c18ba41af5b0507d33783e66d1e7e
mbaddar <mbaddar2@gmail.com>
2022-03-17 10:05:05 +0100

    wrapped memory file logger into a method

--- examples/reporting_logger.py
+++ examples/reporting_logger.py
@@ -1,24 +1,48 @@
-from memory_profiler import profile
+import datetime
+import sys
+
+from memory_profiler import profile, LogFile
 import logging
 
-# create logger
-logger = logging.getLogger('memory_profile_log')
-logger.setLevel(logging.DEBUG)
 
-# create file handler which logs even debug messages
-fh = logging.FileHandler("memory_profile.log")
-fh.setLevel(logging.DEBUG)
+# # create logger
+# logger = logging.getLogger('memory_profile_log')
+# logger.setLevel(logging.DEBUG)
+#
+# # create file handler which logs even debug messages
+# fh = logging.FileHandler("memory_profile.log")
+# fh.setLevel(logging.DEBUG)
+#
+# # create formatter
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# fh.setFormatter(formatter)
+#
+# # add the handlers to the logger
+# logger.addHandler(fh)
+#
+# from memory_profiler import LogFile
+# import sys
+#
+# sys.stdout = LogFile('memory_profile_log', reportIncrementFlag=False)
 
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
 
-# add the handlers to the logger
-logger.addHandler(fh)
+def create_memory_profiler_logger(logger_name, log_file_name, logging_level, log_format):
+    # create logger
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging_level)
 
-from memory_profiler import LogFile
-import sys
-sys.stdout = LogFile('memory_profile_log', reportIncrementFlag=False)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(log_file_name)
+    fh.setLevel(logging_level)
+
+    # create formatter
+    formatter = logging.Formatter(log_format)
+    fh.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    sys.stdout = LogFile(logger_name)
+
 
 @profile
 def my_func():
@@ -27,6 +51,7 @@ def my_func():
     del b
     return a
 
+
 @profile
 def my_func1():
     a = [2] * (10 ** 6)
@@ -34,6 +59,13 @@ def my_func1():
     del b
     return a
 
+
 if __name__ == '__main__':
+    logger_name = 'my_memory_profiler_logger'
+    logfile_name = f"memory_log_{datetime.datetime.now().isoformat()}.log"
+    logging_level = logging.DEBUG
+    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    create_memory_profiler_logger(logger_name=logger_name, log_file_name=logfile_name, logging_level=logging_level,
+                                  log_format=log_format)
     my_func()
     my_func1()
